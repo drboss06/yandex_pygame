48ac15277b6768b599f71d52eb09c6346c72d54c
sadfun <32015025+sadfun@users.noreply.github.com>
2021-01-26 10:24:34 -0700

    Maze ready!

--- setings.py
+++ setings.py
@@ -5,6 +5,10 @@ height = 800
 half_width = width // 2
 half_height = height // 2
 
+# Maze
+maze_length = 33
+maze_width = 33
+
 fov = math.pi / 3
 half_fov = fov / 2
 num_rays = 120
@@ -14,12 +18,16 @@ dist = num_rays / (2 * math.tan((half_fov)))
 proj_co = 3 * dist * 100
 scale = width // num_rays
 
+DOUBLE_PI = math.tau
+CENTER_RAY = num_rays//2 - 1
+FAKE_RAYS = 100
+
 tx_width = 1200
 tx_hight = 1200
 tx_scale = tx_width // 100
 
 player_pos = (112, 112)
 player_angle = 0
-player_speed = 15
+player_speed = 8
 
-fps = 30
+fps = 60
